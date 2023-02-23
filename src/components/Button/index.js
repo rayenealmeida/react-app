eaa3291319b5145a7d85c097bb3fa5b6d409c9af
almeidarayene <eneyar.rayene@gmail.com>
2023-02-23 11:39:10 -0300

    button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,5 @@
-export default function Button({title}) {
-  
-  
-    return (
-    <button>{title}</button>
-  )
+export default function Button(props) {
+    const { title } = props;
+
+    return (<button>{title}</button>);
 }
