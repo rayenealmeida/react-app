85438d7bb9084bd18a62f4e7c81a4edaee7f6d36
almeidarayene <eneyar.rayene@gmail.com>
2023-02-23 11:16:23 -0300

    class

---
+++ src/AppClass.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export default class AppClass extends Component{
+    constructor(props){
+        super(props);
+        this.state = { counter: 0};
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        // TODO
+    }
+    render() {
+        return <div>AppClass</div>;
+    }
+} 
